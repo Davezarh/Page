9bbc49f639983955ef9c21bf876db744603da0d7
Dawid Harendarz <68152541+Davezarh@users.noreply.github.com>
2021-04-14 11:58:14 +0200

    Create routing, and is our root js for whole app.

---
+++ src/views/Root.js
@@ -0,0 +1,28 @@
+import { GlobalStyle } from 'assets/Styles/globalStyle';
+import { theme } from 'assets/Styles/theme';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Wrapper } from './Root.styled';
+import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
+import Home from 'views/Home';
+const Root = () => (
+  <Router>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <MainTemplate>
+        <Wrapper>
+          <Switch>
+            <Route path="/nasiona"></Route>
+            <Route path="/menu"></Route>
+            <Route path="/kontakt"></Route>
+            <Route path="/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </Wrapper>
+      </MainTemplate>
+    </ThemeProvider>
+  </Router>
+);
+export default Root;
