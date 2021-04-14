804cb64aeb5bae9e465c64bdaf6b88f900c46bfd
Dawid Harendarz <68152541+Davezarh@users.noreply.github.com>
2021-04-14 11:57:09 +0200

    Created home as our main view.

---
+++ src/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Wrapper, StyledTitle, StyledQuote } from './Home.styled';
+const Home = () => {
+  return (
+    <>
+      <Wrapper></Wrapper>
+      <StyledTitle>CoffeLovers</StyledTitle>
+      <StyledQuote>
+        Piję kawę i wyobrażam<br></br> sobie brzask gdzie indziej.
+      </StyledQuote>
+    </>
+  );
+};
+export default Home;
