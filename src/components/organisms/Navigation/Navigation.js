b0092fbdce3fa9858b86ededc5477cc0582463c4
Dawid Harendarz <68152541+Davezarh@users.noreply.github.com>
2021-04-14 11:57:31 +0200

    Add navigation to our page.

---
+++ src/components/organisms/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyledLink, Wrapper } from './Navigation.style';
+
+const Navigation = () => {
+  return (
+    <Wrapper>
+      <StyledLink to="/home">Home</StyledLink>
+      <StyledLink to="/nasiona">Nasiona</StyledLink>
+      <StyledLink to="/menu">Menu</StyledLink>
+      <StyledLink to="/kontakt">Kontakt</StyledLink>
+    </Wrapper>
+  );
+};
+export default Navigation;
---
+++ src/components/organisms/Navigation/Navigation.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const Wrapper = styled.nav`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+`;
+export const StyledLink = styled(NavLink)`
+  margin: 10px 20px;
+  padding: 15px;
+  text-decoration: none;
+  font-size: ${({ theme }) => theme.fonts.l};
+  color: ${({ theme }) => theme.colors.black};
+`;
