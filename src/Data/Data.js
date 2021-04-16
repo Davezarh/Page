254e0cc597689495c2c150ff94a3afa201b99bfe
Dawid Harendarz <68152541+Davezarh@users.noreply.github.com>
2021-04-16 14:38:18 +0200

    Create data for menus

---
+++ src/Data/Data.js
@@ -0,0 +1,24 @@
+export const Coffee = [
+  {
+    name: 'Americano',
+    content: 'Aromatyczna kawa pobudzi do działania bogactwem smakowych doznań',
+  },
+  {
+    name: 'Cappuccino',
+    content:
+      'Pobudzające połączenie espresso, spienionego mleka, mlecznej pianki oraz posypki czekoladowej.',
+  },
+  {
+    name: 'Espresso',
+    content: 'a',
+  },
+  {
+    name: 'Flat White',
+    content: 's',
+  },
+  {
+    name: 'Latte',
+    content:
+      'Klasyczna kawa mleczna o intensywnym smaku z charakterystyczną dużą ilością mlecznej pianki',
+  },
+];
