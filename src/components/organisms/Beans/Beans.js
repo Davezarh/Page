c3abc8463c4ff7107d1167ed3629ca11aceafb94
Dawid Harendarz <68152541+Davezarh@users.noreply.github.com>
2021-05-04 11:20:17 +0200

    Splitted whole subpage on smalls components

--- src/components/organisms/Beans/Beans.js
+++ src/components/organisms/Beans/Beans.js
@@ -1,47 +1,16 @@
 import React from 'react';
-import {
-  Header,
-  DarkRoastedImage,
-  LightRoastedImage,
-  MediumRoastedImage,
-  Wrapper,
-  StyledText,
-  StyledHeader,
-} from './Beans.style';
+import RoastedBeans from 'components/molecules/RoastedBeans/RoastedBeans';
+import TypesOfBeans from 'components/molecules/TypesOfBeans/TypesOfBeans';
+import { Header, Wrapper } from './Beans.style';
 
 const Beans = () => {
   return (
-    <>
-      <Header>Jaka Kawa jest najlepsza dla ciebie?</Header>
-      <Wrapper>
-        <LightRoastedImage />
-        <StyledHeader>
-          Jasno palone <span>196 °C - 205 °C</span>
-        </StyledHeader>
-        <StyledText>
-          Lżejszy, wyższa kwasowość, nie oczywisty smak pieczeni ten poziom
-          pieczeni jest idealny do degustacji pełnego pochodzenia kawy.
-        </StyledText>
-        <MediumRoastedImage />
-        <StyledHeader>
-          Średnio palone <span>210 °C - 219 °C</span>
-        </StyledHeader>
-        <StyledText>
-          Cukry uległy dalszej karmelizacji, a kwasowość została wyciszona.
-          Dzięki temu uzyskuje się kawę o wyższej zawartości, ale z pewną
-          ilością narzuconego smaku palenia.
-        </StyledText>
-        <DarkRoastedImage />
-        <StyledHeader>
-          Ciemno palone <span>225 °C - 245 °C</span>
-        </StyledHeader>
-        <StyledText>
-          Wyczuwalne są słodko-gorzkie smaki, wyraźnie zauważalny aromat i smak
-          pieczeni. Niewiele, jeśli w ogóle, pozostało z nich oryginalnego
-          charakteru.
-        </StyledText>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Header>Wybierz rodzaj nasiona dla twojego napoju.</Header>
+      <TypesOfBeans></TypesOfBeans>
+      <Header>Jaki Stopień palenia perefrujesz?</Header>
+      <RoastedBeans></RoastedBeans>
+    </Wrapper>
   );
 };
 export default Beans;
